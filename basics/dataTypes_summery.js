92d29e09281f238ddba2c719107ee5d6e9d32b74
Vaibhav Suthar <108567401+sutharvaibhav@users.noreply.github.com>
2023-11-22 14:46:42 +0000

    dataTypes_summery

---
+++ basics/dataTypes_summery.js
@@ -0,0 +1,19 @@
+// JavaScript is dynamically typed language means we dont have to define the data type it automatically detects data type,
+// Data-types are categorized on how data is stored in the memory and how it is accessed.
+
+// Primitive : Call by value (Stack memory is used)
+//             String, Number, Boolean, Null, Undefined, Symbol, BigInt
+
+const name = "Vaibhav";
+// console.log(typeof name);
+
+const age = 23;
+// console.log(typeof age);
+
+const present = false;
+// console.log(typeof present);
+
+const temp = null;
+// console.log(typeof temp);    
+// *null is object data type*
+
