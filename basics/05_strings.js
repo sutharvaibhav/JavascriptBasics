130c11808775d192047520b0cfbc852ecfba775f
Vaibhav Suthar <108567401+sutharvaibhav@users.noreply.github.com>
2023-11-22 17:06:27 +0000

    strings

---
+++ basics/05_strings.js
@@ -0,0 +1,28 @@
+const name = "Vaibhav";
+const repoCount = 50;
+
+// console.log(name + repoCount + "Value");
+// console.log(`Hello my name is ${name} and repos are ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com');
+// console.log(gameName[6]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('s'));
+
+const newString = gameName.substring(0,4);
+// console.log(newString);
+
+const anotherString = gameName.slice(2, 4);
+// console.log(anotherString);
+
+const newStringOne = "  VAIBHAV     ";
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://hitesh.com/hitesh%20choudhary"
+// console.log(url.replace('%20', '-'))
+// console.log(url.includes('sundar'))
+// console.log(gameName.split('-'));
--- basics/dataTypes_summery.js
+++ basics/dataTypes_summery.js
@@ -5,6 +5,7 @@
 //             String, Number, Boolean, Null, Undefined, Symbol, BigInt
 
 const name = "Vaibhav";
+const userName = "Vaibhav";
 // console.log(typeof name);
 
 const age = 23;
@@ -16,4 +17,52 @@ const present = false;
 const temp = null;
 // console.log(typeof temp);    
 // *null is object data type*
+
+let undef;
+// console.log(typeof undef);
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+// console.log(id === anotherId);
+// two diffrent symbol with same value aren't same 
+
+const bigNum = 98765n;
+// console.log(typeof bigNum);
+
+// Reference (Non primitive) -> call by reference
+// Array, Objects, Functions -> Heap Memory is used
+
+const students = ["Vaibhav", "Adil", "Nisha"]
+// console.log(typeof students);
+// *array is object type of datatype*
+
+const myObj = {
+    name : "Vaibhav",
+    age : 23,
+}
+// console.log(typeof myObj);
+
+const myFun = function() {
+    console.log("Hello JavaScript!");
+}
+// console.log(typeof myFun);
+
+
+// *****************************************
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myName = "vaibhavsuthar";
+let anotherName = myName;
+anotherName = "sutharvaibhav";
+// console.log(myName);
+// console.log(anotherName);
+
+let userOne = {
+    email : "user@google.com",
+    upi : "user@ibl"
+}
+let userTwo = userOne;
+userTwo.email = "vaibhav@google.com";
+// console.log(userOne.email);
+// console.log(userTwo.email);
 
