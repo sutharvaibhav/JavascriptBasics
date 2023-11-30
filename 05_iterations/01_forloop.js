9e7670a4f4946f34205bc749d419aade4adfce57
Vaibhav Suthar <108567401+sutharvaibhav@users.noreply.github.com>
2023-11-30 04:40:57 +0000

    forloop

---
+++ 05_iterations/01_forloop.js
@@ -0,0 +1,12 @@
+// for loop
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i,"\n");
+// }
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer loop value : ${i}`);
+//     for (let j = 0; j < 10; j++) {
+//         console.log(`Inner loop value ${j} and outer loop ${i}`);
+//     }
+// }
